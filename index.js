c6371663b5185d6960baa879922493011892ed25
Pursuit <pursuit@Pursuits-Air.lan>
2020-09-12 22:25:09 -0400

    testing

--- index.js
+++ index.js
@@ -2,21 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3002;
 
-// const { promisify } = require("util");
-// var redis = require("redis");
-// var client = redis.createClient();
+const { promisify } = require("util");
+var redis = require("redis");
+var client = redis.createClient();
 
-// const { job } = require("./worker/worker.js");
-// job.start();
+const { job } = require("./worker/worker.js");
+job.start();
 
-// const getAsync = promisify(client.get).bind(client);
+const getAsync = promisify(client.get).bind(client);
 
 app.get("/jobs", async (req, res) => {
-  // const gJobs = JSON.parse(await getAsync("github"));
-  // //const jJobs = JSON.parse(await getAsync("jooble"));
-  // res.header("Access-Control-Allow-Origin", "*");
-  // return res.send(gJobs);
-  res.send("hi");
+  const gJobs = JSON.parse(await getAsync("github"));
+  //const jJobs = JSON.parse(await getAsync("jooble"));
+  res.header("Access-Control-Allow-Origin", "*");
+  return res.send(gJobs);
 });
 app.listen(port, () => {
   console.log(`listening on port: ${port}`);
